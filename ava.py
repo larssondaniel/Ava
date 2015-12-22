c9e8652182640c0ea44ff46f751b2e6d9323d12e
Daniel Larsson <daniel.larsson@ctk.se>
2015-12-22 14:05:04 +0100

    Try different voice recognition

--- ava.py
+++ ava.py
@@ -7,6 +7,9 @@ from threading import Timer
 # import pyaudio
 # from AppKit import NSSpeechSynthesizer
 import os
+from collections import deque
+import math
+import audioop
 
 try:
   import apiai
@@ -22,6 +25,25 @@ import scipy.io.wavfile as wav
 # CHANNELS = 1
 # RATE = 44100
 # RECORD_SECONDS = 2
+
+RECORD_SECONDS = 2
+
+# Microphone stream config.
+CHUNK = 1024  # CHUNKS of bytes to read each time from mic
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 16000
+THRESHOLD = 2500  # The threshold intensity that defines silence
+                  # and noise signal (an int. lower than THRESHOLD is silence).
+
+SILENCE_LIMIT = 1  # Silence limit in seconds. The max ammount of seconds where
+                   # only silence is recorded. When this time passes the
+                   # recording finishes and the file is delivered.
+
+PREV_AUDIO = 0.5  # Previous audio (in seconds) to prepend. When noise
+                  # is detected, how much of previously recorded audio is
+                  # prepended. This helps to prevent chopping the beggining
+                  # of the phrase.
 
 CLIENT_ACCESS_TOKEN = '9b077f65bd9549abbf8dff4b9c58ea77'
 SUBSCRIPTION_KEY = 'a15b908f-ce1b-4527-a2ec-86097bc57e12'
@@ -97,6 +119,102 @@ def main():
       # os.system("say '" + answer + "'")
 
 
+          # listen_for_speech(THRESHOLD)
+    # p = pyaudio.PyAudio()
+
+    # stream = p.open(format=FORMAT,
+    #                 channels=CHANNELS,
+    #                 rate=RATE,
+    #                 input=True,
+    #                 output=False,
+    #                 frames_per_buffer=CHUNK,
+    #                 stream_callback=callback)
+
+    # stream.start_stream()
+
+    # print ("Say!")
+
+    # try:
+    #     while stream.is_active():
+    #         time.sleep(0.1)
+    # except Exception:
+    #     raise e
+    # except KeyboardInterrupt:
+    #     pass
+
+    # stream.stop_stream()
+    # stream.close()
+    # p.terminate()
+
+    # print ("Wait for response...")
+    # response = request.getresponse()
+
+    # # Convert bytes to string type and string type to dict
+    # string = response.read().decode('utf-8')
+    # json_obj = json.loads(string)
+    # answer = json_obj['result']['fulfillment']['speech']
+    # os.system("say '" + answer + "'")
+    # print(response.read())
+
+def listen_for_speech(threshold=THRESHOLD):
+    """
+    Listens to Microphone, extracts phrases from it and sends it to
+    Google's TTS service and returns response. a "phrase" is sound
+    surrounded by silence (according to threshold). num_phrases controls
+    how many phrases to process before finishing the listening process
+    (-1 for infinite).
+    """
+    #Open stream
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+
+    print "* Listening mic. "
+    audio2send = []
+    cur_data = ''  # current chunk  of audio data
+    rel = RATE/CHUNK
+    slid_win = deque(maxlen=SILENCE_LIMIT * rel)
+    #Prepend audio from 0.5 seconds before noise was detected
+    prev_audio = deque(maxlen=PREV_AUDIO * rel)
+    started = False
+    response = []
+
+    while True:
+        cur_data = stream.read(CHUNK)
+        slid_win.append(math.sqrt(abs(audioop.avg(cur_data, 4))))
+        #print slid_win[-1]
+        if(sum([x > THRESHOLD for x in slid_win]) > 0):
+            if(not started):
+                print "Starting record of phrase"
+                started = True
+            audio2send.append(cur_data)
+        elif (started is True):
+            print "Finished"
+            # The limit was reached, finish capture and deliver.
+            filename = save_speech(list(prev_audio) + audio2send, p)
+            # Send file to Google and get response
+            r = stt_google_wav(filename)
+
+            # Reset all
+            started = False
+            slid_win = deque(maxlen=SILENCE_LIMIT * rel)
+            prev_audio = deque(maxlen=0.5 * rel)
+            audio2send = []
+            print "Listening ..."
+        else:
+            prev_audio.append(cur_data)
+
+    print "* Done recording"
+    stream.close()
+    p.terminate()
+
+    return response
+
+
 class Router:
 
   def handle_intent(self, intent, parameters):
@@ -108,9 +226,6 @@ class Router:
     method = getattr(reminders, str(method_name))
     # Call the method as we return it
     method(parameters)
-
-  # def reminders(self):
-  #   print "Reminder!!!"
 
 if __name__ == '__main__':
   main()
